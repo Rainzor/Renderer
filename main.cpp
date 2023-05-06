168f055f9d3fdd9bbba9420132931792dc0ffc41
Rainzor <runze.wang02@outlook.com>
2023-05-06 18:46:45 +0800

    Complete in one week

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #include "moving_sphere.h"
 #include "sphere.h"
 
-#define NUM_THREADS 16  // 线程数
+#define NUM_THREADS 8  // 线程数
 
 color ray_color(const ray& r, const color& background, const hittable& world);
 color ray_color(const ray& r, const color& background, const hittable& world, int depth);
@@ -27,6 +27,7 @@ hittable_list earth();
 hittable_list simple_light();
 hittable_list cornell_box();
 hittable_list cornell_smoke();
+hittable_list final_scene();
 int main() {
     using namespace std::chrono;
     // Parallel
@@ -99,13 +100,23 @@ int main() {
             lookat = pointf3(278, 278, 0);
             vfov = 40.0;
             break;
-        default:
         case 7:
             world = cornell_smoke();
             aspect_ratio = 1.0;
             image_width = 600;
             samples_per_pixel = 200;
             lookfrom = pointf3(278, 278, -800);
+            lookat = pointf3(278, 278, 0);
+            vfov = 40.0;
+            break;
+        default:
+        case 8:
+            world = final_scene();
+            aspect_ratio = 1.0;
+            image_width = 800;
+            samples_per_pixel = 10000;
+            background = color(0, 0, 0);
+            lookfrom = pointf3(478, 278, -600);
             lookat = pointf3(278, 278, 0);
             vfov = 40.0;
             break;
@@ -165,8 +176,11 @@ int main() {
 
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
+    auto minu = duration_cast<minutes>(end - start);
+    auto sec = duration_cast<seconds>(end - start)-minu*60;
+
     std::cerr << std::endl
-              << "Time Cost:" << duration.count() << " ms" << std::endl;
+              << "Time Cost:" << minu.count() << "m" << sec.count() << "s" << std::endl;
 
     // for (int j = image_height - 1; j >= 0; --j) {
     //     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
@@ -388,6 +402,66 @@ hittable_list cornell_smoke() {
 
     objects.add(make_shared<constant_medium>(box1, 0.01, color(0, 0, 0)));
     objects.add(make_shared<constant_medium>(box2, 0.01, color(1, 1, 1)));
+
+    return objects;
+}
+hittable_list final_scene() {
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+    const int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++) {
+        for (int j = 0; j < boxes_per_side; j++) {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i * w;
+            auto z0 = -1000.0 + j * w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = random_double(1, 101);
+            auto z1 = z0 + w;
+
+            boxes1.add(make_shared<box>(pointf3(x0, y0, z0), pointf3(x1, y1, z1), ground));
+        }
+    }
+
+    hittable_list objects;
+
+    objects.add(make_shared<bvh_node>(boxes1, 0, 1));
+
+    auto light = make_shared<diffuse_light>(color(7, 7, 7));
+    objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
+
+    auto center1 = pointf3(400, 400, 200);
+    auto center2 = center1 + vecf3(30, 0, 0);
+    auto moving_sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+    objects.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
+
+    objects.add(make_shared<sphere>(pointf3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+    objects.add(make_shared<sphere>(
+        pointf3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)));
+
+    auto boundary = make_shared<sphere>(pointf3(360, 150, 145), 70, make_shared<dielectric>(1.5));
+    objects.add(boundary);
+    objects.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
+    boundary = make_shared<sphere>(pointf3(0, 0, 0), 5000, make_shared<dielectric>(1.5));
+    objects.add(make_shared<constant_medium>(boundary, .0001, color(1, 1, 1)));
+
+    auto emat = make_shared<lambertian>(make_shared<image_texture>("img/earthmap.jpg"));
+    objects.add(make_shared<sphere>(pointf3(400, 200, 400), 100, emat));
+    auto pertext = make_shared<noise_texture>(0.1);
+    objects.add(make_shared<sphere>(pointf3(220, 280, 300), 80, make_shared<lambertian>(pertext)));
+
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(make_shared<sphere>(pointf3::random(0, 165), 10, white));
+    }
+
+    objects.add(make_shared<translate>(
+        make_shared<rotate_y>(
+            make_shared<bvh_node>(boxes2, 0.0, 1.0), 15),
+        vecf3(-100, 270, 395)));
 
     return objects;
 }
